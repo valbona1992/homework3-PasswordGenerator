3d76bc190f70d8c1ac230d615f2307b74f863918
valbona1992 <valbona12@gmail.com>
2021-06-22 08:57:34 -0500

    Add code to javascript file and updated README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # homework3-PasswordGenerator
+
+
+## Summary
+This assignment required me to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected.  
+
+Below is a summary of what I implemented. 
+
+* Added function to generate password that provided prompts for the user to select multiple password criteria.
+* Added variables for the criterias including numbers, special characters, lowercase characters, and uppercase characters.
+* Added alerts if the password did not meet the length criteria and if no criteria was selected.
+* Created a loop that runs as many times as the password length provided by the user.
+* Selected a random characters and added to the password holder as many times as indicated in the loop.
+* The function returns the final password which is displayed in the browser.
+
+## Screenshot
+![screenshot](Assets/PasswordGenerator.png)
--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  let passwordLength = prompt("How many characters would you like your password to be ?");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please select a password length of at least 8 characters and no more than 128 characters!");
+    return "";
+  }
+
+  let specialCharacters = confirm("Click OK to confirm using special characters");
+  let numericCharacters = confirm("Click OK to confirm using numeric characters");
+  let lowercaseCharacters = confirm("Click OK to confirm using lowercase characters");
+  let uppercaseCharacters = confirm("Click OK to confirm using uppercase characters");
+
+  if (!specialCharacters && !numericCharacters && !lowercaseCharacters && !uppercaseCharacters) {
+    alert("Please select at least one character type!");
+    return "";
+  }
+
+  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let special = ["!",'"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', ']', "^", '_', "`", "{", "|", "}", "~" ];
+  let lowercase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+  let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+
+  let validChoices = [];
+
+  if (specialCharacters){
+    validChoices = validChoices.concat(special);
+  } 
+
+  if (numericCharacters) {
+    validChoices = validChoices.concat(numbers);
+  }
+
+  if (lowercaseCharacters) {
+    validChoices = validChoices.concat(lowercase);
+  }
+
+  if (uppercaseCharacters) {
+    validChoices = validChoices.concat(uppercase);
+  }
+
+  let password = "";
+  for (let i =0; i < passwordLength; i++) {
+    let myrandom = Math.floor (Math.random () * validChoices.length);
+    password += validChoices[myrandom];
+  }
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +62,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
